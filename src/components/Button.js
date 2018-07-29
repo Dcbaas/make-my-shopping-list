150002343702614a5466e74cdd73d0f6908f257b
David Baas <David@Rogers-MacBook-Pro.local>
2018-07-29 19:34:13 -0400

    Wrote Comments For the Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
-const Button = () => {
+/**
+ * The Button facilitate action within the Application
+ * 
+ * @author David Baas
+ * @version 1.0 - 7/30/2018
+ * @since 1.0 - 7/30/2018
+ */
+
+/**
+ * The Button facilitates various actions within the Application. It is lightweight with only its
+ * style specified while the action and the text inside must be spcified. 
+ * @param {object} props 
+ */
+const Button = (props) => {
     return (
        <TouchableOpacity 
             style={styles.buttonStyle}
-            onPress={() => console.log(this)}      
+            onPress={props.onPress}      
        >
            <Text style={styles.textStyle}>
                Click Me
