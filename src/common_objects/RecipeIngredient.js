bb3675b09a879d53dedb92eb8799213537de137a
David Baas <David@Rogers-MacBook-Pro.local>
2018-08-07 15:37:05 -0400

    Wrote Comments for RecipeIngredint.js

--- src/common_objects/RecipeIngredient.js
+++ src/common_objects/RecipeIngredient.js
@@ -1,5 +1,24 @@
 
+/**
+ * The RecipeIngredient Class is a unified class that tracks all the data for individual Ingredients
+ * in the Recipe Screen. It tracks its index in the array, its name, its amount, and its units.
+ * The class only has two methods of the constructor and a toString method for displaying it in a
+ * list.
+ * 
+ * @author David Baas
+ * @version 1.0 - 8/7/2018
+ * @since 1.0 8/7/2018
+ */
 class RecipeIngredient {
+
+    /**
+     * The constructor initializes all variables.
+     * 
+     * @param {number} index The index of where this RecipeIngredient is in the array.
+     * @param {string} name The name of this RecipeIngredint.
+     * @param {number} amount The amount of this RecipeIngredint.
+     * @param {*} units the units for this RecipeIngredint.
+     */
     constructor(index, name, amount, units) {
         this.index = index;
         this.name = name;
@@ -7,6 +26,12 @@ class RecipeIngredient {
         this.units = units;
     }
     
+    /**
+     * The toString method returns a formatted string with information about this RecipeIngredint.
+     * This will be used in the list on the Recipe screen.
+     * 
+     * @returns A formatted string for this RecipeIngredint.
+     */
     toString() {
         return `${this.amount} ${this.units} of ${this.name}`;
     }
