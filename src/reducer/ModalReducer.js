59f099de65d4bc17689af29c97711837c373f5d8
David Baas <David@Rogers-MacBook-Pro.local>
2018-08-07 13:09:34 -0400

    unfinished ModalReducer

---
+++ src/reducer/ModalReducer.js
@@ -0,0 +1,20 @@
+import { CURRENT_INGREDIENT_NEW,
+    CURRENT_INGREDIENT_UPDATE,
+    CURRENT_INGREDIENT_NAME_CHANGED,
+    CURRENT_INGREDIENT_AMOUNT_CHANGED, 
+    CURRENT_INGREDIENT_UNITS_CHANGED } from '../actions/types';
+/**
+ * The ModalReducer file contains all the state and state changes for the Modal in the RecipeScreen.
+ * 
+ * @author David Baas
+ * @version 0.1 - 8/7/2018
+ * @since 0.1 - 8/7/2018
+ */
+
+const INITAL_STATE = {
+    ingredient: null
+};
+
+export default (state = INITAL_STATE, action) => {
+    
+}
