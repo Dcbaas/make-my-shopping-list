f8563fca1b575d4a303fb0aa30a57925731a887b
David Baas <David@Rogers-MacBook-Pro.local>
2018-08-09 19:33:45 -0400

    Wrote Head comments

--- src/screens/RecipeScreen.js
+++ src/screens/RecipeScreen.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+/**
+ * The RecipeScreen is where a recipe is created to be used in a weekly meal plan
+ * 
+ * @author David Baas
+ * @version 0.1 - 8/9/2018
+ * @since 0.1 - 8/9/2018
+ */
+
+ 
 const RecipeScreen = () => {
     return (
         <View>
