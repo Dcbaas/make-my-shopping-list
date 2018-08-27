c685017a7ceef67088cfd418fec8fa123937d386
David Baas <David@Rogers-MacBook-Pro.local>
2018-08-27 18:34:47 -0400

    Setup Firebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Firebase from 'firebase';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
@@ -6,6 +7,18 @@ import reducers from './reducer';
 import Navigation from './Navigation';
 
 export default class App extends React.Component {
+
+  componentWillMount() {
+    Firebase.initializeApp({
+      apiKey: 'AIzaSyCy2QbBxR-cIbODkcjx3UTqTsF94wPKQMM',
+      authDomain: 'make-my-shopping-list-48b9a.firebaseapp.com',
+      databaseURL: 'https://make-my-shopping-list-48b9a.firebaseio.com',
+      projectId: 'make-my-shopping-list-48b9a',
+      storageBucket: 'make-my-shopping-list-48b9a.appspot.com',
+      messagingSenderId: '545531973470'
+    });
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}> 
